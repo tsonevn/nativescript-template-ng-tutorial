e551c4b19ee7b7175e08d0164d02757be3a75e96
TJ VanToll <tj.vantoll@gmail.com>
2017-01-12 15:25:35 -0500

    Use an ActionBar by default

--- app.component.ts
+++ app.component.ts
@@ -3,6 +3,7 @@ import { Component } from "@angular/core";
 @Component({
   selector: "my-app",
   template: `
+    <ActionBar title="My App"></ActionBar>
     <!-- Your UI components go here -->
   `
 })
