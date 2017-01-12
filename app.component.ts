64bd9c52e667fdce37f03bdf0d8278b7a206acbc
TJ VanToll <tj.vantoll@gmail.com>
2017-01-12 15:03:33 -0500

    Initial commit

---
+++ app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "my-app",
+  template: `
+    <!-- Your UI components go here -->
+  `
+})
+export class AppComponent {
+  // Your TypeScript logic goes here
+}
