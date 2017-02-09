ce4ee716cf004222ffab9d92d1d5002bd7c40207
sis0k0 <stanimira.vlaeva@gmail.com>
2017-02-09 18:59:46 +0200

    feat: support AoT compilation with nativescript-dev-webpack plugin

--- main.ts
+++ main.ts
@@ -1,15 +1,4 @@
-import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { platformNativeScriptDynamic } from "nativescript-angular/platform";
-import { NativeScriptModule } from "nativescript-angular/nativescript.module";
-
-import { AppComponent } from "./app.component";
-
-@NgModule({
-  declarations: [AppComponent],
-  bootstrap: [AppComponent],
-  imports: [NativeScriptModule],
-  schemas: [NO_ERRORS_SCHEMA]
-})
-export class AppModule {}
+import { AppModule } from "./app.module";
 
 platformNativeScriptDynamic().bootstrapModule(AppModule);
