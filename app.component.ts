f5c3fadcaf31d7cf64f478049e42bccdbd1450c3
Nikolay Tsonev <ntsonev@mcsofntsonev.bedford.progress.com>
2017-03-23 11:07:46 +0200

    add missing actionbar class

--- app.component.ts
+++ app.component.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 @Component({
   selector: "my-app",
   template: `
-    <ActionBar title="My App"></ActionBar>
+    <ActionBar title="My App" class="action-bar"></ActionBar>
     <!-- Your UI components go here -->
   `
 })
